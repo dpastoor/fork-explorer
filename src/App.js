dcc16a9463ecc736060ddbe98fce0d050b380447
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-04 13:36:40 -0800

    trying to figure out querying github api

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {Slider, DropDownMenu, RaisedButton} from 'material-ui';
 import _ from 'lodash';
+import request from 'browser-request';
+import axios from 'axios';
+import superagent from 'superagent';
 export class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {counter: 0}
     }
+    componentWillMount() {
+        axios.get('https://raw.githubusercontent.com/dpastoor/awesome-shiny/master/README.md')
+            .then(function (response) {
+                console.log(response);
+            });
+    }
+
   render() {
+    setTimeout(() => {
+        this.setState({counter: this.state.counter+1})
+    }, 1000)
+
     return (
+
        <div>
-           <h1>hello world</h1>
+           <h1>hello how are you doing {this.state.counter}</h1>
+
        </div>
     );
   }
