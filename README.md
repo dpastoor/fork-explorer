8ef4db0579c620779e2c356d6640fd6f75bb0961
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-04 11:23:54 -0800

    initialize with boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+n-queens
+======
+
+
+refactored n-queens implementation with chess board based to visualize the various solver algorithms
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {Slider, DropDownMenu, RaisedButton} from 'material-ui';
+import _ from 'lodash';
+export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+  render() {
+    return (
+       <div>
+           <h1>hello world</h1>
+       </div>
+    );
+  }
+}
+
+
+
