557014825d168e209c3a0fa7ca10ab0d4d220414
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-05 19:09:07 -0800

    routing example from react router to pick apart

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export class App extends Component {
+export class Login extends Component {
     constructor(props) {
         super(props);
     }
   render() {
-
     return (
        <div>
-           <h1> Fork Explorer</h1>
-           {console.log('in login')}
-               {this.props.children}
+           <h1>Login</h1>
        </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import { render } from 'react-dom'
+import { createHistory, useBasename } from 'history'
+import { Router, Route, Link } from 'react-router'
 
-render(<App />, document.getElementById('root'));
+
+const history = useBasename(createHistory)({
+    basename: '/breadcrumbs'
+});
+
+class App extends React.Component {
+    render() {
+        const depth = this.props.routes.length
+
+        return (
+            <div>
+                <aside>
+                    <ul>
+                        <li><Link to={"/products"}>Products</Link></li>
+                        <li><Link to={Orders.path}>Orders</Link></li>
+                    </ul>
+                </aside>
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+        )
+    }
+}
+
+App.title = 'Home'
+App.path = '/'
+
+
+class Products extends React.Component {
+    render() {
+        return (
+            <div className="Page">
+                <h1>Products</h1>
+            </div>
+        )
+    }
+}
+
+
+class Orders extends React.Component {
+    render() {
+        return (
+            <div className="Page">
+                <h1>Orders</h1>
+            </div>
+        )
+    }
+}
+
+Orders.title = 'Orders'
+Orders.path = '/orders'
+
+render((
+    <Router history={history}>
+        <Route path={App.path} component={App}>
+            <Route path={"/products"} component={Products} />
+            <Route path={Orders.path} component={Orders} />
+        </Route>
+    </Router>), document.getElementById('root'));
