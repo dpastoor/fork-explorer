1e16ad956a7562111d6a86c526209996ce9d115f
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-10 15:27:02 -0800

    add authentication and storage of token in localstorage

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,12 +1,30 @@
 
 import React from 'react';
+
 import { render } from 'react-dom'
+import xhr from 'xhr'
 export default class Repos extends React.Component {
+
+    _authenticate() {
+        const code = this.props.location.query.code;
+        xhr({
+            url: 'https://fork-explorer-lh.herokuapp.com/authenticate/' + code,
+            json: true
+        }, (err, req, body) => {
+            console.log('body');
+            window.localStorage.token = body.token;
+        });
+    }
+    componentWillMount() {
+        if (!window.localStorage.token || window.localStorage.token === 'undefined') {
+            this._authenticate()
+        }
+    }
     render() {
 
         return (
             <div>
-                <h1> callback gave the code: {this.props.location.query.code} </h1>
+                <h1> Welcome! </h1>
             </div>
         )
     }
