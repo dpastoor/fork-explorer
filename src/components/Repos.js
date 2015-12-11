c3437348a0a6237cee3c74c4012f7393256cc5f2
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-10 17:35:22 -0800

    grabbing a file yeaaaaaah

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import qs from 'qs';
 import xhr from 'xhr';
 import {RaisedButton} from 'material-ui';
 export default class Repos extends React.Component {
@@ -36,12 +37,28 @@ export default class Repos extends React.Component {
             console.log(body)
         });
     }
+    _fetchUserFile(params) {
+        xhr({
+            url: 'https://api.github.com/repos/hackreactor/2015-11-toy-problems/contents/primeTester/primeTester.js',
+            json: true,
+            headers: {
+                Authorization: 'token ' + window.localStorage.token
+            }
+        }, (err, req, body) => {
+            console.log('data fetching successful');
+            console.log('req')
+            console.log(req)
+            console.log('body')
+            console.log(body)
+        });
+    }
     render() {
 
         return (
             <div>
                 <h1> Welcome! </h1>
                 <RaisedButton label="fetch user data" onClick={this._fetchUserData}/>
+                <RaisedButton label="fetch file data" onClick={this._fetchUserFile}/>
             </div>
         )
     }
