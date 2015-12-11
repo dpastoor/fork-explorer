3a74f4548fd255f12e13e989900f45c7c592e254
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-11 08:58:59 -0800

    update dependencies for npm3 and get rid of extraneous product component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom'
 import { createHistory, useBasename } from 'history'
 import { Router, Route, Link } from 'react-router'
-import Products from './components/Products'
 import Login from './components/Login'
 import Repos from './components/Repos'
 import App from './App'
