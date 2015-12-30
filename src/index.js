26e59ae5b4a1964cca0955ff9ef4e53a41a67524
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-29 20:34:52 -0800

    clean up old example code

--- src/index.js
+++ src/index.js
@@ -8,18 +8,6 @@ import App from './App'
 const history = useBasename(createHistory)({
     basename: '/'
 });
-
-
-class Orders extends React.Component {
-    render() {
-        return (
-            <div className="Page">
-                <h1>input 2</h1>
-            </div>
-        )
-    }
-}
-
 render((
     <Router history={history}>
         <Route path={"/"} component={App}>
